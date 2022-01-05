b7756c0e5b94f791835cedc0482ca0083fb9a7d6
wenhao.lei <ruihaobwn@163.com>
2022-01-05 16:02:27 +0800

    增加库存预警过滤

--- repo/filters/ShopFilter.py
+++ repo/filters/ShopFilter.py
@@ -1,15 +1,20 @@
 from django_filters import rest_framework as filters
-
+from django.db.models import F
 from repo.models import Shop, ShopRecord
 
 
 class ShopFilter(filters.FilterSet):
     shop_name = filters.CharFilter(lookup_expr='icontains')
     shop_no= filters.CharFilter(lookup_expr='icontains')
+    lte_total = filters.NumberFilter(field_name='total_num', method='total_lt_threshold')
+
+    def total_lt_threshold(self, queryset, name, value):
+        queryset = Shop.objects.filter(total_num__lte=F('threshold'))
+        return queryset
 
     class Meta:
         model = Shop
-        fields = ['shop_no', 'shop_name']
+        fields = ['shop_no', 'shop_name', 'lte_total']
 
 
 class ShopRecordFilter(filters.FilterSet):
